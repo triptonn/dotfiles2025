8b1fa9badc5f2d4907615f691aec19806c7988ed
Nicolas Selig <triptonn.dev@gmail.com>
2025-02-22 20:12:23 +0100

    v0.115: Implemented toggle for autocomplete and streamlined snacks plugin

--- nvim/.config/nvim/lua/template/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/template/plugins/nvim-cmp.lua
@@ -22,9 +22,34 @@ return {
 		local luasnip = require("luasnip")
 		local lspkind = require("lspkind")
 
+		-- Create a variable to track completion state
+		vim.g.cmp_enabled = true
+
+		-- Function to toggle completion
+		local toggle_completion = function()
+			vim.g.cmp_enabled = not vim.g.cmp_enabled
+			if vim.g.cmp_enabled then
+				cmp.setup.buffer({
+					enabled = true,
+				})
+				print("Autocompletion enabled")
+			else
+				cmp.setup.buffer({
+					enabled = false,
+				})
+				print("Autocompletion diabled")
+			end
+		end
+
+		-- Keybinding for toggle cmp
+		vim.keymap.set("n", "<leader>ta", toggle_completion, { desc = "Toggle autocompletion" })
+
 		-- Loads vs-code style snippets from installed plugins (e.g. friendly-snippets)
 		require("luasnip.loaders.from_vscode").lazy_load()
 		cmp.setup({
+			enabled = function()
+				return vim.g.cmp_enabled
+			end,
 			performance = {
 				debounce = 60,
 				throttle = 30,
@@ -32,8 +57,6 @@ return {
 				max_view_entries = 100,
 			},
 			completion = {
-				-- border = "rounded",
-				-- scrollbar = "║",
 				keyword_length = 1, -- Start completion after 1 character
 				completeopt = "menu,menuone,noselect",
 			},
--- nvim/.config/nvim/lua/template/plugins/snacks.lua
+++ nvim/.config/nvim/lua/template/plugins/snacks.lua
@@ -1,166 +1,44 @@
 return {
 	"folke/snacks.nvim",
 	priority = 1000,
-	lazy = false,
-	---@type snacks.Config
+	lazy = true,
+	event = "VeryLazy",
 	opts = {
 		bigfile = { enabled = false },
 		dashboard = { enabled = false },
-		dim = { enabled = true },
 		explorer = { enabled = false },
 		indent = { enabled = false },
 		input = { enabled = false },
-		notifier = { enabled = true, timeout = 5000 },
-		notify = { enabled = true },
 		picker = { enabled = false },
 		quickfile = { enabled = false },
-		scope = {
-			enabled = true,
-			-- absolute minimum size of the scope.
-			-- can be less if the scope is a top-level single line scope
-			min_size = 2,
-			-- try to expand the scope to this size
-			max_size = nil,
-			cursor = true, -- when true, the column of the cursor is used to determine the scope
-			edge = true, -- include the edge of the scope (typically the line above and below with smaller indent)
-			siblings = false, -- expand single line scopes with single line siblings
-			-- what buffers to attach to
-			filter = function(buf)
-				return vim.bo[buf].buftype == ""
-			end,
-			-- debounce scope detection in ms
-			debounce = 30,
-			treesitter = {
-				-- detect scope based on treesitter.
-				-- falls back to indent based detection if not available
-				enabled = true,
-				injections = true, -- include language injections when detecting scope (useful for languages like `vue`)
-				---@type string[]|{enabled?:boolean}
-				blocks = {
-					enabled = false, -- enable to use the following blocks
-					"function_declaration",
-					"function_definition",
-					"method_declaration",
-					"method_definition",
-					"class_declaration",
-					"class_definition",
-					"do_statement",
-					"while_statement",
-					"repeat_statement",
-					"if_statement",
-					"for_statement",
-				},
-				-- these treesitter fields will be considered as blocks
-				field_blocks = {
-					"local_declaration",
-				},
-			},
-			-- These keymaps will only be set if the `scope` plugin is enabled.
-			-- Alternatively, you can set them manually in your config,
-			-- using the `Snacks.scope.textobject` and `Snacks.scope.jump` functions.
-			keys = {
-				---@type table<string, snacks.scope.TextObject|{desc?:string}>
-				textobject = {
-					ii = {
-						min_size = 2, -- minimum size of the scope
-						edge = false, -- inner scope
-						cursor = false,
-						treesitter = { blocks = { enabled = false } },
-						desc = "inner scope",
-					},
-					ai = {
-						cursor = false,
-						min_size = 2, -- minimum size of the scope
-						treesitter = { blocks = { enabled = false } },
-						desc = "full scope",
-					},
-				},
-				---@type table<string, snacks.scope.Jump|{desc?:string}>
-				jump = {
-					["[i"] = {
-						min_size = 1, -- allow single line scopes
-						bottom = false,
-						cursor = false,
-						edge = true,
-						treesitter = { blocks = { enabled = false } },
-						desc = "jump to top edge of scope",
-					},
-					["]i"] = {
-						min_size = 1, -- allow single line scopes
-						bottom = true,
-						cursor = false,
-						edge = true,
-						treesitter = { blocks = { enabled = false } },
-						desc = "jump to bottom edge of scope",
-					},
-				},
-			},
-		},
-		scratch = {
-			-- enabled = true,
-			name = "Scratch",
-			ft = function()
-				if vim.bo.buftype == "" and vim.bo.filetype ~= "" then
-					return vim.bo.filetype
-				end
-				return "markdown"
-			end,
-			icon = nil, -- `icon|{icon, icon_hl}`. defaults to the filetype icon
-			root = vim.fn.stdpath("data") .. "/scratch",
-			autowrite = true, -- automatically write when the buffer is hidden
-			filekey = {
-				cwd = true, -- use current working directory
-				branch = true, -- use current branch name
-				count = true, -- use vim.v.count1
-			},
-			win = { style = "scratch", relative = "editor" },
-			win_by_ft = {
-				lua = {
-					keys = {
-						["source"] = {
-							"<cr>",
-							function(self)
-								local name = "scratch." .. vim.fn.fnamemodify(vim.api.nvim_buf_get_name(self.buf), ":e")
-								Snacks.debug.run({ buf = self.buf, name = name })
-							end,
-							desc = "Source buffer",
-							mode = { "n", "x" },
-						},
-					},
-				},
-			},
-		},
-		scroll = { enabled = true },
 		statuscolumn = { enabled = false },
 
+		notifier = { enabled = true, timeout = 3000 },
+		notify = { enabled = true },
+
+		scope = {
+			enabled = true,
+			min_size = 2,
+			debounce = 50,
+			treesitter = {
+				enabled = true,
+				injection = false,
+				blocks = {
+					enabled = false,
+				},
+			},
+		},
 		words = {
-			-- enabled = true,
-			debounce = 300, -- time in ms to wait before updating
+			enabled = true,
+			debounce = 500, -- time in ms to wait before updating
 			notify_jump = false, -- show a notification when jumping
-			notify_end = true, -- show a notification when reaching the end
-			foldopen = true, -- open folds after jumping
+			notify_end = false, -- show a notification when reaching the end
+			foldopen = false, -- open folds after jumping
 			jumplist = true, -- set jump point before jumping
-			modes = { "n", "i", "c" }, -- modes to show references
+			modes = { "n" }, -- modes to show references n, i, ...
 		},
 	},
 	keys = {
-
-		-- scratch keymappings
-		{
-			"<leader>.",
-			function()
-				Snacks.scratch()
-			end,
-			desc = "Toggle Scratch Buffer",
-		},
-		{
-			"<leader>S",
-			function()
-				Snacks.scratch.select()
-			end,
-			desc = "Select Scratch Buffer",
-		},
-
 		-- words keymappings
 		{
 			"ö",
@@ -177,27 +55,12 @@ return {
 			desc = "󰉚 Prev reference",
 		},
 		{
-			"<leader>g?",
-			function()
-				require("snacks").git.blame_line()
-			end,
-			desc = " Blame line",
-		},
-		{
 			"<leader>hh",
 			function()
 				require("snacks").notifier.show_history()
 			end,
 			mode = { "n", "v", "i" },
 			desc = "󰎟 Last notification",
-		},
-		{
-			"<leader>om",
-			function()
-				local enabled = require("snacks").dim.enabled
-				require("snacks").dim[enabled and "disable" or "enable"]()
-			end,
-			desc = "󰝟 Mute code",
 		},
 	},
 }
