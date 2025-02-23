61a401095fb16211fa5e2afaf0a528434357b478
Nicolas Selig <triptonn.dev@gmail.com>
2025-02-23 14:34:15 +0100

    v0.117: Ordered colorscheme

--- nvim/.config/nvim/lua/template/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/template/plugins/colorscheme.lua
@@ -4,19 +4,13 @@ return {
 	config = function()
 		local transparent = true
 
-		-- local bg = "#011628"
 		local bg = "#1A1B26"
-		-- local bg_dark = "#011423"
 		local bg_dark = "#16161E"
-		-- local bg_highlight = "#143652"
 		local bg_highlight = "#292E42"
 		local bg_search = "#0A64AC"
 		local bg_visual = "#275378"
-		-- local fg = "#CBE0F0"
 		local fg = "#C0CAF5"
-		-- local fg_dark = "#B4D0E9"
 		local fg_dark = "#A9B1D6"
-		-- local fg_gutter = "#627E97"
 		local fg_gutter = "#3B4261"
 		local border = "#547998"
 
