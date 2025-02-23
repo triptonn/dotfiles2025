e44f304db434cd4a2556f180253a475d7b256455
Nicolas Selig <triptonn.dev@gmail.com>
2025-02-23 16:41:07 +0100

    v0.121: Made autocompletions give far more possible options

--- nvim/.config/nvim/lua/template/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/template/plugins/nvim-cmp.lua
@@ -4,17 +4,17 @@ return {
 	dependencies = {
 		"hrsh7th/cmp-buffer", -- source for text in buffer
 		"hrsh7th/cmp-path", -- source for filesystem paths
-		--"hrsh7th/cmp-nvim-lsp", -- source for LSP
-		--"hrsh7th/cmp-nvim-lua", -- source for nvim lua
+		"hrsh7th/cmp-nvim-lsp", -- source for LSP
+		"hrsh7th/cmp-nvim-lua", -- source for nvim lua
 		"L3MON4D3/LuaSnip",
-		--"saadparwaiz1/cmp_luasnip", -- for autocompletion
-		--"rafamadriz/friendly-snippets", -- useful snippets
-		--"onsails/lspkind.nvim", -- vs-code like pictograms
+		"saadparwaiz1/cmp_luasnip", -- for autocompletion
+		"rafamadriz/friendly-snippets", -- useful snippets
+		"onsails/lspkind.nvim", -- vs-code like pictograms
 	},
 	config = function()
 		local cmp = require("cmp")
-		--local luasnip = require("luasnip")
-		--local lspkind = require("lspkind")
+		local luasnip = require("luasnip")
+		local lspkind = require("lspkind")
 
 		-- Create a variable to track completion state
 		vim.g.cmp_enabled = true
@@ -62,14 +62,14 @@ return {
 				},
 			}, ]]
 			completion = {
-				keyword_length = 2, -- Start completion after 2 character
+				keyword_length = 1, -- Start completion after 2 character
 				completeopt = "menu,menuone,noselect,noinsert",
 			},
-			--[[ snippet = {
+			snippet = {
 				expand = function(args)
 					luasnip.lsp_expand(args.body)
 				end,
-			}, ]]
+			},
 			mapping = cmp.mapping.preset.insert({
 				["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
 				["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
@@ -92,8 +92,8 @@ return {
 						return require("cmp.types").lsp.CompletionItemKind[entry:get_kind()] ~= "Text"
 					end,
 				},
-				--{ name = "nvim_lua" }, -- nvim lua
-				--{ name = "luasnip" }, -- snippets
+				{ name = "nvim_lua" }, -- nvim lua
+				{ name = "luasnip" }, -- snippets
 			}),
 			preselect = cmp.PreselectMode.None, -- Don't preselect first item
 
@@ -101,14 +101,14 @@ return {
 			formatting = {
 				fields = { "kind", "abbr" },
 				--expandable_indicator = true,
-				--[[ format = lspkind.cmp_format({
+				format = lspkind.cmp_format({
 					maxwidth = 50,
 					ellipsis_char = "...",
-				}), ]]
-				format = function(entry, vim_item)
+				}),
+				--[[ format = function(entry, vim_item)
 					vim_item.kind = string.sub(vim_item.kind, 1, 1)
 					return vim_item
-				end,
+				end, ]]
 			},
 		})
 	end,
